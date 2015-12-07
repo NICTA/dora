7c70f69aeef90413b1727106a45f8eddac4a0efd
KelvyHsu <Kelvin.Hsu@nicta.com.au>
2015-12-07 17:31:13 +1100

    Added unit test for 'GaussianProcess' sampler

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,1 @@
 import pytest
-
-
-@pytest.fixture
-def setup_delaunay():
-
-    lower = [0, 0]
-    upper = [1, 1]
-
-    n_samples = 10
-    explore_priority = 0.0001
-
-    return lower, upper, n_samples, explore_priority
--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -10,31 +10,35 @@ def equality(a, b, tol=1e-4):
 
 
 def ground_truth(x):
-    return (np.sum((x - 0.5) ** 2) < 0.1).astype(float)
+    return (np.sum((x - 0.5) ** 2, axis=-1) < 0.1).astype(float)
 
 
-def test_delaunay(setup_delaunay):
+def test_delaunay():
 
-    lower, upper, n_samples, explore_priority = setup_delaunay
+    np.random.seed(100)
+
+    lower = [0, 0]
+    upper = [1, 1]
+
+    n_samples = 10
+    explore_priority = 0.0001
 
     sampler = sampling.Delaunay(lower, upper,
                                 explore_priority=explore_priority)
 
-    np.random.seed(100)
     xq, uid = sampler.pick()
 
     assert equality(xq, np.array([0, 0]))
     assert uid == 'e99bf8c47ac0cebc187804b541ba1ac6'
 
     yq = ground_truth(xq)
-    ind = sampler.update(uid, yq)
-    assert ind == 0
+    assert sampler.update(uid, yq) == 0
 
     for i in range(n_samples - 1):
 
         xq, uid = sampler.pick()
-        observation = ground_truth(xq)
-        sampler.update(uid, observation)
+        yq = ground_truth(xq)
+        sampler.update(uid, yq)
 
     assert equality(np.array(sampler.X), np.array([
                     [0., 0.],
@@ -47,3 +51,46 @@ def test_delaunay(setup_delaunay):
                     [0.75018708, 0.5],
                     [0.5, 0.75018708],
                     [0.87518701, 0.5]]))
+
+
+def test_gp():
+
+    np.random.seed(100)
+
+    lower = [0, 0]
+    upper = [1, 1]
+
+    n_samples = 10
+    n_initial_sample = 50
+
+    X_train = sampling.random_sample(lower, upper, n_initial_sample)
+    y_train = ground_truth(X_train)
+
+    sampler = sampling.GaussianProcess(lower, upper, X_train, y_train,
+                                       add_train_data=False)
+
+    xq, uid = sampler.pick()
+
+    assert equality(xq, np.array([0, 0]))
+    assert uid == '99970dae22a50c6210333d05b581762e'
+
+    yq = ground_truth(xq)
+    assert sampler.update(uid, yq) == 0
+
+    for i in range(n_samples - 1):
+
+        xq, uid = sampler.pick()
+        yq = ground_truth(xq)
+        sampler.update(uid, yq)
+
+    assert equality(np.array(sampler.X), np.array([
+                    [0., 0.],
+                    [1., 0.],
+                    [0., 1.],
+                    [1., 1.],
+                    [0.5, 0.5],
+                    [0.36052525, 0.54937526],
+                    [0.40648024, 0.34281852],
+                    [0.2526294, 0.31233231],
+                    [0.20180019, 0.56829352],
+                    [0.48608311, 0.67256845]]))
