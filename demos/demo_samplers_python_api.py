41e472584c50f88034ea35c835be7e418e4956b8
socallaghan <simon.ocallaghan@nicta.com.au>
2016-02-22 16:43:13 +1100

    Reduce number of print statements in demos.

--- demos/demo_samplers_python_api.py
+++ demos/demo_samplers_python_api.py
@@ -22,27 +22,33 @@ from demos.example_processes import simulate_measurement
 
 import matplotlib.pyplot as pl
 
+from time import sleep
 
-def main(sampling_method='GaussianProcess', plot=False):
+def main(sampler_model='GaussianProcess', plot=False):
 
     # Set up a sampling problem
     lower = [0, 0]
     upper = [1, 1]
     n_samples = 301
 
+    logging.info('\n \n Sampler Model: %s' % sampler_model)
+    sleep(3)
+
+
     # Initialise the sampler
-    if sampling_method == 'GaussianProcess':
+    if sampler_model == 'GaussianProcess':
         acq_name = 'sigmoid'
-        n_train = 49
+        n_train = 50
         sampler = sampling.GaussianProcess(lower, upper, acq_name=acq_name,
                                            n_train=n_train, seed=100)
-    elif sampling_method == 'Delaunay':
+    elif sampler_model == 'Delaunay':
+
         explore_priority = 0.0001
         sampler = sampling.Delaunay(lower, upper,
                                     explore_priority=explore_priority)
     else:
         raise ValueError('Sampling method "%s" not implemented yet'
-                         % sampling_method)
+                         % sampler_model)
 
     # Set up plotting
     if plot:
@@ -50,33 +56,46 @@ def main(sampling_method='GaussianProcess', plot=False):
         n_triggers = len(plot_triggers)
         plt_size = pltutils.split_subplots(n_triggers)
         fig = pl.figure()
+        pl.title(sampler_model)
         axs = iter([fig.add_subplot(*(plt_size + (i + 1,)))
                     for i in range(n_triggers)])
 
     # Start active sampling!
     for i in range(n_samples):
 
+        # Log the iteration number
+        if divmod(i,100)[1] == 0:
+            log_info = True
+            logging.info('Iteration: %d' % i)
+
         # Pick a location to sample
+        if log_info:
+            logging.info('Picking new observation location..')
         xq, uid = sampler.pick()
 
         # Sample that location
+        if log_info:
+            logging.info('Evaluating observation value...')
         yq_true = simulate_measurement(xq)
 
         # Update the sampler about the new observation
+        if log_info:
+            logging.info('Updating sampler... \n \n ')
+            log_info = False
+            sleep(2)
         sampler.update(uid, yq_true)
 
         # Plot the sampler progress
         if plot and i in plot_triggers:
             pltutils.plot_sampler_progress(sampler, next(axs))
 
-        # Log the iteration number
-        logging.info('Iteration: %d' % i)
+
 
     # Sampler demos must return the sampler itself
     return sampler
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    main(sampling_method='Delaunay', plot=True)
-    main(sampling_method='GaussianProcess', plot=True)
+    main(sampler_model='Delaunay', plot=True)
+    main(sampler_model='GaussianProcess', plot=True)
     pl.show()
