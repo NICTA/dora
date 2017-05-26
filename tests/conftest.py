0d673662b427c6f84b65dfc0936e060b4b10ea7c
Dave Cole <david.cole@data61.csiro.au>
2017-05-26 10:22:02 +1000

    Fix pytest warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,13 @@
 PyTest Configuration.
 """
 
+import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--update-ref-data", action="store", default=False,
                      help="Set to True if reference data is to be updated")
 
 
-def pytest_funcarg__update_ref_data(request):
+@pytest.fixture()
+def update_ref_data(request):
     return request.config.option.update_ref_data
